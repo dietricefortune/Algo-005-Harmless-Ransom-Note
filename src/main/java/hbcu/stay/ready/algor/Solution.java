4ff52d34c8c379fa5f60968c062a1cb697034fd0
Dietrice Fortune <dietricefortune@Dietrices-MacBook-Pro.local>
2019-08-06 09:26:21 -0400

    test case failed but im in here baby! let me take another stab at it

--- src/main/java/hbcu/stay/ready/algor/Solution.java
+++ src/main/java/hbcu/stay/ready/algor/Solution.java
@@ -1,8 +1,33 @@
 package hbcu.stay.ready.algor;
 
+import java.util.HashMap;
+
 public class Solution {
 
-    public Boolean harmlessRansomNote(String message , String magazineText){
-        return null;
+    public Boolean harmlessRansomNote(String message, String magazineText) {
+
+            //String sentence = "";
+            String[] note1 = message.split(" ");
+
+            //initialize the hash map and call it ransom1
+            HashMap<String, String> noteText = new HashMap<>();
+
+            //if the ransom's VALUE does not return null, return false because something is repeated
+            for (String str : note1) {
+                if (noteText.get(str) != null) {
+                    return false;
+
+                //if not false, the return true, duh
+                } else {
+
+                    return true;
+
+                }
+
+            }
+            return true || false;
+        }
+
+
+
     }
-}
